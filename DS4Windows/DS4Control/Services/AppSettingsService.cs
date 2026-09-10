c607441c904271201e0c59a43b22e32320e79b15
gwin7ok <gwin7ok@gmail.com>
2026-09-11 01:32:58 +0900

    refactor: Phase5-Step14 AppSettingsService を Global アクセサパターンに統一し不要プロパティを削除 ＊ビルド、テストビルド、テスト実行全て成功

--- DS4Windows/DS4Control/Services/AppSettingsService.cs
+++ DS4Windows/DS4Control/Services/AppSettingsService.cs
@@ -12,45 +12,47 @@ namespace DS4Windows.Services
         // 正規シムとし、UI層(MainWindow.xaml.cs等)からGlobal直参照した場合と完全に同一の実体を
         // 参照するようにする。従来はここに独立したprivateフィールドを持ち、Global側と非連動という
         // 「孤立した重複状態」バグがあった(Step13投入時に発覚)。
-        // CheckWhenのみ、対応するGlobal側の概念が既に撤廃されているため、
+        // CheckWhenのみ、対応するGlobal側の概念が既に撤廃されている(コメント "Legacy CheckWhen
+        // removed. Use explicit CheckEveryValue/CheckEveryUnit instead." 参照)ため、
         // 単体テストのみで参照される非連動プロパティとして現状維持する。
-        private int _checkWhen = 24;
+        private int _checkWhen;
 
         public event EventHandler<string> SettingChanged;
 
         public AppSettingsService(IProfileXmlStore xmlStore = null, IPathService pathService = null)
         {
-            _xmlStore = xmlStore;
-            _pathService = pathService;
+            _xmlStore = xmlStore ?? DS4WinWPF.AppHost.GetService<IProfileXmlStore>() ?? new ProfileXmlStore();
+            _pathService = pathService ?? DS4WinWPF.AppHost.GetService<IPathService>() ?? new PathService();
         }
 
         public bool Save()
         {
-            try
+            bool success = _xmlStore.SaveAppSettingsXml();
+            if (success)
             {
-                return _xmlStore != null ? _xmlStore.SaveAppSettingsXml(_pathService?.GetProfilesFilePath()) : Global.Save();
+                if (AppLogger.IsTraceEnabled)
+                    AppLogger.LogTrace("[DI] AppSettingsService.Save succeeded");
             }
-            catch
+            else
             {
-                return false;
+                AppLogger.LogToGui("Failed to save application settings", true);
+                if (AppLogger.IsTraceEnabled)
+                    AppLogger.LogTrace("[DI] AppSettingsService.Save failed");
             }
+            return success;
         }
 
         public bool Load()
         {
-            try
-            {
-                return _xmlStore != null ? _xmlStore.LoadAppSettingsXml(_pathService?.GetProfilesFilePath()) : Global.Load();
-            }
-            catch
-            {
-                return false;
-            }
+            bool success = _xmlStore.LoadAppSettingsXml();
+            if (AppLogger.IsTraceEnabled)
+                AppLogger.LogTrace($"[DI] AppSettingsService.Load: success={success}");
+            return success;
         }
 
-        public void NotifyChanged(string propertyName = null)
+        private void NotifyChanged(string settingName)
         {
-            SettingChanged?.Invoke(this, propertyName);
+            SettingChanged?.Invoke(this, settingName);
         }
 
         public bool StartMinimized
@@ -81,12 +83,12 @@ namespace DS4Windows.Services
 
         public bool CloseMinimizes
         {
-            get => Global.CloseMinimizes;
+            get => Global.CloseMini;
             set
             {
-                if (Global.CloseMinimizes != value)
+                if (Global.CloseMini != value)
                 {
-                    Global.CloseMinimizes = value;
+                    Global.CloseMini = value;
                     NotifyChanged(nameof(CloseMinimizes));
                 }
             }
@@ -107,12 +109,12 @@ namespace DS4Windows.Services
 
         public bool UseUdpServer
         {
-            get => Global.IsUsingUDPServer();
+            get => Global.isUsingUDPServer();
             set
             {
-                if (Global.IsUsingUDPServer() != value)
+                if (Global.isUsingUDPServer() != value)
                 {
-                    Global.SetUseUDPServer(value);
+                    Global.setUsingUDPServer(value);
                     NotifyChanged(nameof(UseUdpServer));
                 }
             }
@@ -125,7 +127,7 @@ namespace DS4Windows.Services
             {
                 if (Global.getUDPServerPortNum() != value)
                 {
-                    Global.setUDPServerPortNum(value);
+                    Global.setUDPServerPort(value);
                     NotifyChanged(nameof(UdpServerPort));
                 }
             }
@@ -170,314 +172,152 @@ namespace DS4Windows.Services
             }
         }
 
-        // ---- Phase5-Step14前クリーンアップ: MainWindow.xaml.cs残存Global参照の解消 ----
-        public string LogMinLevel
-        {
-            get => Global.LogMinLevel.ToString();
-            set
-            {
-                if (int.TryParse(value, out var lvl))
-                {
-                    if (Global.LogMinLevel != lvl)
-                    {
-                        Global.LogMinLevel = lvl;
-                        NotifyChanged(nameof(LogMinLevel));
-                    }
-                }
-            }
-        }
-
-        public bool CheckUpdateStartupEnabled
-        {
-            get => Global.CheckUpdateStartup;
-            set
-            {
-                if (Global.CheckUpdateStartup != value)
-                {
-                    Global.CheckUpdateStartup = value;
-                    NotifyChanged(nameof(CheckUpdateStartupEnabled));
-                }
-            }
-        }
-
-        public int CheckEveryValue
-        {
-            get => Global.CheckEvery;
-            set
-            {
-                if (Global.CheckEvery != value)
-                {
-                    Global.CheckEvery = value;
-                    NotifyChanged(nameof(CheckEveryValue));
-                }
-            }
-        }
-
-        public int CheckEveryUnit
-        {
-            get => Global.CheckEveryUnit;
-            set
-            {
-                if (Global.CheckEveryUnit != value)
-                {
-                    Global.CheckEveryUnit = value;
-                    NotifyChanged(nameof(CheckEveryUnit));
-                }
-            }
-        }
-
-        public DateTime LastChecked
-        {
-            get => Global.LastChecked;
-            set
-            {
-                if (Global.LastChecked != value)
-                {
-                    Global.LastChecked = value;
-                    NotifyChanged(nameof(LastChecked));
-                }
-            }
-        }
-
-        public ulong LastVersionCheckedNum
-        {
-            get => Global.LastVersionCheckedNumber;
-        }
-
-        public bool FirstRun
-        {
-            get => Global.FirstRun;
-            set
-            {
-                if (Global.FirstRun != value)
-                {
-                    Global.FirstRun = value;
-                    NotifyChanged(nameof(FirstRun));
-                }
-            }
-        }
-
-        public bool RunHotPlug
-        {
-            get => Global.RunHotPlug;
-            set
-            {
-                if (Global.RunHotPlug != value)
-                {
-                    Global.RunHotPlug = value;
-                    NotifyChanged(nameof(RunHotPlug));
-                }
-            }
-        }
-
         // ---- Phase5-Step13-7: ウィンドウ位置・サイズ、コントローラー一覧列幅の永続化 ----
+        // いずれも m_Config(BackingStore) への薄い公開アクセサ(Global.X)への正規シム。
         public int FormWidth
         {
             get => Global.FormWidth;
-            set
-            {
-                if (Global.FormWidth != value)
-                {
-                    Global.FormWidth = value;
-                    NotifyChanged(nameof(FormWidth));
-                }
-            }
+            set { if (Global.FormWidth != value) { Global.FormWidth = value; NotifyChanged(nameof(FormWidth)); } }
         }
 
         public int FormHeight
         {
             get => Global.FormHeight;
-            set
-            {
-                if (Global.FormHeight != value)
-                {
-                    Global.FormHeight = value;
-                    NotifyChanged(nameof(FormHeight));
-                }
-            }
+            set { if (Global.FormHeight != value) { Global.FormHeight = value; NotifyChanged(nameof(FormHeight)); } }
         }
 
         public int FormLocationX
         {
             get => Global.FormLocationX;
-            set
-            {
-                if (Global.FormLocationX != value)
-                {
-                    Global.FormLocationX = value;
-                    NotifyChanged(nameof(FormLocationX));
-                }
-            }
+            set { if (Global.FormLocationX != value) { Global.FormLocationX = value; NotifyChanged(nameof(FormLocationX)); } }
         }
 
         public int FormLocationY
         {
             get => Global.FormLocationY;
-            set
-            {
-                if (Global.FormLocationY != value)
-                {
-                    Global.FormLocationY = value;
-                    NotifyChanged(nameof(FormLocationY));
-                }
-            }
+            set { if (Global.FormLocationY != value) { Global.FormLocationY = value; NotifyChanged(nameof(FormLocationY)); } }
         }
 
         public int ControllerIndexColWidth
         {
             get => Global.ControllerIndexColWidth;
-            set
-            {
-                if (Global.ControllerIndexColWidth != value)
-                {
-                    Global.ControllerIndexColWidth = value;
-                    NotifyChanged(nameof(ControllerIndexColWidth));
-                }
-            }
+            set { if (Global.ControllerIndexColWidth != value) { Global.ControllerIndexColWidth = value; NotifyChanged(nameof(ControllerIndexColWidth)); } }
         }
 
         public int ControllerIdColWidth
         {
             get => Global.ControllerIdColWidth;
-            set
-            {
-                if (Global.ControllerIdColWidth != value)
-                {
-                    Global.ControllerIdColWidth = value;
-                    NotifyChanged(nameof(ControllerIdColWidth));
-                }
-            }
+            set { if (Global.ControllerIdColWidth != value) { Global.ControllerIdColWidth = value; NotifyChanged(nameof(ControllerIdColWidth)); } }
         }
 
         public int ControllerStatusColWidth
         {
             get => Global.ControllerStatusColWidth;
-            set
-            {
-                if (Global.ControllerStatusColWidth != value)
-                {
-                    Global.ControllerStatusColWidth = value;
-                    NotifyChanged(nameof(ControllerStatusColWidth));
-                }
-            }
+            set { if (Global.ControllerStatusColWidth != value) { Global.ControllerStatusColWidth = value; NotifyChanged(nameof(ControllerStatusColWidth)); } }
         }
 
         public int ControllerExclusiveColWidth
         {
             get => Global.ControllerExclusiveColWidth;
-            set
-            {
-                if (Global.ControllerExclusiveColWidth != value)
-                {
-                    Global.ControllerExclusiveColWidth = value;
-                    NotifyChanged(nameof(ControllerExclusiveColWidth));
-                }
-            }
+            set { if (Global.ControllerExclusiveColWidth != value) { Global.ControllerExclusiveColWidth = value; NotifyChanged(nameof(ControllerExclusiveColWidth)); } }
         }
 
         public int ControllerBatteryColWidth
         {
             get => Global.ControllerBatteryColWidth;
-            set
-            {
-                if (Global.ControllerBatteryColWidth != value)
-                {
-                    Global.ControllerBatteryColWidth = value;
-                    NotifyChanged(nameof(ControllerBatteryColWidth));
-                }
-            }
+            set { if (Global.ControllerBatteryColWidth != value) { Global.ControllerBatteryColWidth = value; NotifyChanged(nameof(ControllerBatteryColWidth)); } }
         }
 
         public int ControllerSelectProfileColWidth
         {
             get => Global.ControllerSelectProfileColWidth;
-            set
-            {
-                if (Global.ControllerSelectProfileColWidth != value)
-                {
-                    Global.ControllerSelectProfileColWidth = value;
-                    NotifyChanged(nameof(ControllerSelectProfileColWidth));
-                }
-            }
+            set { if (Global.ControllerSelectProfileColWidth != value) { Global.ControllerSelectProfileColWidth = value; NotifyChanged(nameof(ControllerSelectProfileColWidth)); } }
         }
 
         public int ControllerEditColWidth
         {
             get => Global.ControllerEditColWidth;
-            set
-            {
-                if (Global.ControllerEditColWidth != value)
-                {
-                    Global.ControllerEditColWidth = value;
-                    NotifyChanged(nameof(ControllerEditColWidth));
-                }
-            }
+            set { if (Global.ControllerEditColWidth != value) { Global.ControllerEditColWidth = value; NotifyChanged(nameof(ControllerEditColWidth)); } }
         }
 
         public int ControllerLinkedProfileColWidth
         {
             get => Global.ControllerLinkedProfileColWidth;
-            set
-            {
-                if (Global.ControllerLinkedProfileColWidth != value)
-                {
-                    Global.ControllerLinkedProfileColWidth = value;
-                    NotifyChanged(nameof(ControllerLinkedProfileColWidth));
-                }
-            }
+            set { if (Global.ControllerLinkedProfileColWidth != value) { Global.ControllerLinkedProfileColWidth = value; NotifyChanged(nameof(ControllerLinkedProfileColWidth)); } }
         }
 
         public int ControllerLinkProfIdColWidth
         {
             get => Global.ControllerLinkProfIdColWidth;
-            set
-            {
-                if (Global.ControllerLinkProfIdColWidth != value)
-                {
-                    Global.ControllerLinkProfIdColWidth = value;
-                    NotifyChanged(nameof(ControllerLinkProfIdColWidth));
-                }
-            }
+            set { if (Global.ControllerLinkProfIdColWidth != value) { Global.ControllerLinkProfIdColWidth = value; NotifyChanged(nameof(ControllerLinkProfIdColWidth)); } }
         }
 
         public int ControllerCustomColorColWidth
         {
             get => Global.ControllerCustomColorColWidth;
-            set
-            {
-                if (Global.ControllerCustomColorColWidth != value)
-                {
-                    Global.ControllerCustomColorColWidth = value;
-                    NotifyChanged(nameof(ControllerCustomColorColWidth));
-                }
-            }
+            set { if (Global.ControllerCustomColorColWidth != value) { Global.ControllerCustomColorColWidth = value; NotifyChanged(nameof(ControllerCustomColorColWidth)); } }
         }
 
         // ---- Phase5-Step13-7: 通知レベル・スワイプ操作設定 ----
         public int Notifications
         {
             get => Global.Notifications;
-            set
-            {
-                if (Global.Notifications != value)
-                {
-                    Global.Notifications = value;
-                    NotifyChanged(nameof(Notifications));
-                }
-            }
+            set { if (Global.Notifications != value) { Global.Notifications = value; NotifyChanged(nameof(Notifications)); } }
         }
 
         public bool SwipeProfiles
         {
             get => Global.SwipeProfiles;
-            set
-            {
-                if (Global.SwipeProfiles != value)
-                {
-                    Global.SwipeProfiles = value;
-                    NotifyChanged(nameof(SwipeProfiles));
-                }
-            }
+            set { if (Global.SwipeProfiles != value) { Global.SwipeProfiles = value; NotifyChanged(nameof(SwipeProfiles)); } }
+        }
+
+        // ---- Phase5-Step14前クリーンアップ: MainWindow.xaml.cs残存Global参照の解消 ----
+        // いずれも Global(m_Config/BackingStore) への薄い公開アクセサへの正規シム。
+        // Notifications 等と同一パターン（独立フィールドを持たない）。
+        public string LogMinLevel
+        {
+            get => Global.LogMinLevel;
+            set { if (Global.LogMinLevel != value) { Global.LogMinLevel = value; NotifyChanged(nameof(LogMinLevel)); } }
+        }
+
+        public bool CheckUpdateStartupEnabled
+        {
+            get => Global.CheckUpdateStartupEnabled;
+            set { if (Global.CheckUpdateStartupEnabled != value) { Global.CheckUpdateStartupEnabled = value; NotifyChanged(nameof(CheckUpdateStartupEnabled)); } }
+        }
+
+        public int CheckEveryValue
+        {
+            get => Global.CheckEveryValue;
+            set { if (Global.CheckEveryValue != value) { Global.CheckEveryValue = value; NotifyChanged(nameof(CheckEveryValue)); } }
+        }
+
+        public int CheckEveryUnit
+        {
+            get => Global.CheckEveryUnit;
+            set { if (Global.CheckEveryUnit != value) { Global.CheckEveryUnit = value; NotifyChanged(nameof(CheckEveryUnit)); } }
+        }
+
+        public DateTime LastChecked
+        {
+            get => Global.LastChecked;
+            set { if (Global.LastChecked != value) { Global.LastChecked = value; NotifyChanged(nameof(LastChecked)); } }
+        }
+
+        // Global.LastVersionCheckedNum は読み取り専用（LastVersionChecked代入時に自動算出）のため、
+        // 本シムも読み取り専用として公開する。
+        public ulong LastVersionCheckedNum => Global.LastVersionCheckedNum;
+
+        public bool FirstRun
+        {
+            get => Global.firstRun;
+            set { if (Global.firstRun != value) { Global.firstRun = value; NotifyChanged(nameof(FirstRun)); } }
+        }
+
+        public bool RunHotPlug
+        {
+            get => Global.runHotPlug;
+            set { if (Global.runHotPlug != value) { Global.runHotPlug = value; NotifyChanged(nameof(RunHotPlug)); } }
         }
 
         // ---- Phase5-Step14: プロファイル編集画面・SpecialAction カラム幅の永続化 ----
