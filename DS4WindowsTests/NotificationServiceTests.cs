fd2aa099cdfe22bb316476db624412357d8392b4
gwin7ok <gwin7ok@gmail.com>
2026-09-11 02:01:39 +0900

    refactor: Phase5-Step14 NotificationServiceTests を Global 同期に更新し状態復元を追加 ＊ビルド、テストビルド、テスト実行全て成功

--- DS4WindowsTests/NotificationServiceTests.cs
+++ DS4WindowsTests/NotificationServiceTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Xunit;
 using DS4Windows;
 using DS4Windows.DI;
@@ -8,51 +8,120 @@ namespace DS4WindowsTests
     public class NotificationServiceTests
     {
         [Fact]
-        public void Defaults_ShouldHaveNotificationsEnabled()
+        public void Defaults_ShouldSynchronizeWithGlobal()
         {
-            var service = new AppNotificationService();
+            var origNotifications = Global.Notifications;
+            var origFlash = Global.FlashWhenLate;
+            try
+            {
+                Global.Notifications = 1;
+                Global.FlashWhenLate = false;
 
-            Assert.True(service.NotificationsEnabled);
-            Assert.False(service.FlashTaskbar);
+                var service = new AppNotificationService();
+
+                Assert.True(service.NotificationsEnabled);
+                Assert.False(service.FlashTaskbar);
+            }
+            finally
+            {
+                Global.Notifications = origNotifications;
+                Global.FlashWhenLate = origFlash;
+            }
         }
 
         [Fact]
         public void SendNotification_ShouldFireNotificationTriggered_WhenEnabled()
         {
-            var service = new AppNotificationService();
-            NotificationEventArgs eventArgs = null;
-            service.NotificationTriggered += (s, e) => eventArgs = e;
+            var origNotifications = Global.Notifications;
+            try
+            {
+                var service = new AppNotificationService();
+                service.NotificationsEnabled = true;
 
-            service.SendNotification("TitleTest", "MessageTest", true);
+                NotificationEventArgs eventArgs = null;
+                service.NotificationTriggered += (s, e) => eventArgs = e;
 
-            Assert.NotNull(eventArgs);
-            Assert.Equal("TitleTest", eventArgs.Title);
-            Assert.Equal("MessageTest", eventArgs.Message);
-            Assert.True(eventArgs.IsToast);
+                service.SendNotification("TitleTest", "MessageTest", true);
+
+                Assert.NotNull(eventArgs);
+                Assert.Equal("TitleTest", eventArgs.Title);
+                Assert.Equal("MessageTest", eventArgs.Message);
+                Assert.True(eventArgs.IsToast);
+            }
+            finally
+            {
+                Global.Notifications = origNotifications;
+            }
         }
 
         [Fact]
         public void SendNotification_ShouldNotFire_WhenDisabled()
         {
-            var service = new AppNotificationService();
-            bool eventFired = false;
-            service.NotificationTriggered += (s, e) => eventFired = true;
+            var origNotifications = Global.Notifications;
+            try
+            {
+                var service = new AppNotificationService();
+                bool eventFired = false;
+                service.NotificationTriggered += (s, e) => eventFired = true;
 
-            service.NotificationsEnabled = false;
-            service.SendNotification("TitleTest", "MessageTest");
+                service.NotificationsEnabled = false;
+                service.SendNotification("TitleTest", "MessageTest");
 
-            Assert.False(eventFired);
+                Assert.False(eventFired);
+                Assert.Equal(0, Global.Notifications);
+            }
+            finally
+            {
+                Global.Notifications = origNotifications;
+            }
         }
 
         [Fact]
         public void GlobalShim_ShouldSynchronizeWithService()
         {
-            var service = new AppNotificationService();
-            Global.NotificationServiceInstance = service;
+            var origFlash = Global.FlashWhenLate;
+            try
+            {
+                var service = new AppNotificationService();
+                Global.NotificationServiceInstance = service;
 
-            Assert.NotNull(Global.NotificationServiceInstance);
-            Global.NotificationServiceInstance.FlashTaskbar = true;
-            Assert.True(service.FlashTaskbar);
+                Assert.NotNull(Global.NotificationServiceInstance);
+                Global.NotificationServiceInstance.FlashTaskbar = true;
+                Assert.True(service.FlashTaskbar);
+                Assert.True(Global.FlashWhenLate);
+            }
+            finally
+            {
+                Global.FlashWhenLate = origFlash;
+            }
+        }
+
+        [Fact]
+        public void Service_ShouldDirectlyReflectGlobalChanges()
+        {
+            var origNotifications = Global.Notifications;
+            var origFlash = Global.FlashWhenLate;
+            try
+            {
+                var service = new AppNotificationService();
+
+                Global.Notifications = 0;
+                Assert.False(service.NotificationsEnabled);
+
+                Global.Notifications = 1;
+                Assert.True(service.NotificationsEnabled);
+
+                Global.FlashWhenLate = true;
+                Assert.True(service.FlashTaskbar);
+
+                Global.FlashWhenLate = false;
+                Assert.False(service.FlashTaskbar);
+            }
+            finally
+            {
+                Global.Notifications = origNotifications;
+                Global.FlashWhenLate = origFlash;
+            }
         }
     }
 }
